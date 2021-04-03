28a69144f40c03b55524e1d40fe1ce86adbdc745
MrCichy94 <przemyslav.cichy@gmail.com>
2021-04-03 10:54:27 +0200

    2021_C3_Create_leftPanel_and_mainPanel_components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ContainerComponent } from './components/container/container.component';
-import {ScriptService} from './components/services/script.service';
+import {AppComponent} from './app.component';
+import {ContainerComponent} from './components/container/container.component';
+import {ScriptService} from './components/services/scriptService/script.service';
+import {LeftPanelComponent} from './components/container/left-panel/left-panel.component';
+import {MainPanelComponent} from './components/container/main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    LeftPanelComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule,
   ],
   providers: [
-    { provide: 'ORIGIN_URL', useValue: location.origin },
+    {provide: 'ORIGIN_URL', useValue: location.origin},
     ScriptService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ScriptService} from '../services/scriptService/script.service';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ContainerComponent implements OnInit {
+
+  constructor(private script: ScriptService) { }
+
+  ngOnInit(): void {
+    this.script.load('TimelineMax', 'TweenMax', 'fullPage', 'app').then(data => {
+      console.log('script loaded ', data);
+    }).catch(error => console.log(error));
+  }
+
+}
