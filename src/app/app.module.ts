844894905697c199d0c56fb1f86d04492dbba3d7
MrCichy94 <przemyslav.cichy@gmail.com>
2021-04-03 12:08:07 +0200

    2021_C4_Create_components_for_leftPanel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import {ContainerComponent} from './components/container/container.component';
 import {ScriptService} from './components/services/scriptService/script.service';
 import {LeftPanelComponent} from './components/container/left-panel/left-panel.component';
 import {MainPanelComponent} from './components/container/main-panel/main-panel.component';
+import { NavbarComponent } from './components/container/main-panel/navbar/navbar.component';
+import { UserImageComponent } from './components/container/left-panel/user-image/user-image.component';
+import { OptionListComponent } from './components/container/left-panel/option-list/option-list.component';
+import { OptionComponent } from './components/container/left-panel/option-list/option/option.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     LeftPanelComponent,
-    MainPanelComponent
+    MainPanelComponent,
+    NavbarComponent,
+    UserImageComponent,
+    OptionListComponent,
+    OptionComponent
   ],
   imports: [
     BrowserModule,
