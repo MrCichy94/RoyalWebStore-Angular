327ac93cd6c86b793ac3872e608dbd4d692dda73
MrCichy94 <przemyslav.cichy@gmail.com>
2021-06-11 09:33:17 +0200

    2021_C38_Fix_validator_for_product_name

--- src/app/components/container/main-panel/content/add/add.component.ts
+++ src/app/components/container/main-panel/content/add/add.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {first} from 'rxjs/operators';
 import {Router} from '@angular/router';
-import {AuthenticationService} from '../../../../../services/authentication/authentication.service';
 import {AlertService} from '../../../../../services/authentication/alert.service';
-import {UserService} from '../../../../../services/userService/user.service';
 import {ProductServiceService} from '../../../../../services/productService/product-service.service';
 
 @Component({
@@ -24,11 +22,12 @@ export class AddComponent implements OnInit {
     private router: Router,
     private alertService: AlertService,
     private productServiceService: ProductServiceService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.addItemForm = this.formBuilder.group({
-      productName: ['', [Validators.required, Validators.pattern('[a-zA-Z]{2,25}')]],
+      productName: ['', [Validators.required, Validators.pattern('.*\\S.*[a-zA-z0-9 ]{2,25}')]],
       sellBaseGrossPrice: ['', [Validators.required, Validators.pattern('^-?[0-9]\\d*(\\.\\d{2})?$')]],
       vatPercentage: ['', [Validators.required, Validators.pattern('^-?[0]\\d*(\\.\\d{2})?$')]]
     });
@@ -62,9 +61,9 @@ export class AddComponent implements OnInit {
 
   public preparePostData(data: string) {
     const stringData = JSON.stringify({
-      productName: this.f.emailLogin.value,
-      sellBaseGrossPrice: this.f.password.value,
-      vatPercentage: this.f.firstName.value
+      productName: this.f.productName.value,
+      sellBaseGrossPrice: this.f.sellBaseGrossPrice.value,
+      vatPercentage: this.f.vatPercentage.value
     });
     console.log(stringData);
     return stringData;
